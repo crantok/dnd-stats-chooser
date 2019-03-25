8b9a9cc40e804363ad40f491e447ca6ef26e29c2
Justin Hellings <justin.hellings@gmail.com>
2019-03-25 19:45:19 +0000

    Added licence headers to code files.

---
+++ build-readme.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+#
+# Copyright 2019 Justin Hellings (info@justinhellings.uk)
+#
+# This file is part of dnd-stats-chooser.
+# The full project can be found at https://github.com/crantok/dnd-stats-chooser
+#
+# dnd-stats-chooser is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# dnd-stats-chooser is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with dnd-stats-chooser. If not, see http://www.gnu.org/licenses/.
+#
+
+# Print out a readme explaining what the programs does and showing its output.
+
+# Single string for introduction to information
+
+# open README.md for write (delete contents)
+#     append introduction
+#     append output of ./scores.rb in a code block
+# close file
--- scores.rb
+++ scores.rb
@@ -1,6 +1,31 @@
 #!/usr/bin/env ruby
+
 #
-require 'pp'
+# Copyright 2019 Justin Hellings (info@justinhellings.uk)
+#
+# This file is part of dnd-stats-chooser.
+# The full project can be found at https://github.com/crantok/dnd-stats-chooser
+#
+# dnd-stats-chooser is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# dnd-stats-chooser is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with dnd-stats-chooser. If not, see http://www.gnu.org/licenses/.
+#
+
+#
+# dnd-stats-chooser - Within the contex of the points-allocation system for
+# choosing ability scores for a D&D 5e character; print out the sets of ability
+# scores (and relevant analysis) that are most points-efficient for different
+# race bonuses.
+#
 
 POINTS = [9,7,5,4,3,2,1,0]
 
